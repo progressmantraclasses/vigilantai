a2a27a4edbdf9ae10d3f0c390f80a40185471388
progressmantraclasses <144003310+progressmantraclasses@users.noreply.github.com>
2025-04-05 20:03:32 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,41 +10,8 @@ import VideoCapture from "./components/VideoCapture";
 import Video from "./components/Video";
 import Avtar from "./components/Avtar";
 import FitbitIntegration from "./components/WearableDevice";
-import IncidentForm from "./components/IncidentForm";
-import IncidentViewer from "./components/IncidentViewer";
 
 function App() {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState([]);
-  const [contract, setContract] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-  const [adminAddress, setAdminAddress] = useState("");
-
-  useEffect(() => {
-    const initialize = async () => {
-      try {
-        const web3Instance = await getWeb3();
-        setWeb3(web3Instance);
-
-        const accounts = await web3Instance.eth.getAccounts();
-        setAccounts(accounts);
-
-        const { instance } = await getContract(web3Instance);
-        setContract(instance);
-
-        const admin = await instance.methods.admin().call();
-        setAdminAddress(admin);
-      } catch (error) {
-        console.error("Failed to load web3, accounts, or contract:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    initialize();
-  }, []);
-
-  if (error) return <div className="error">{error}</div>;
 
   return (
     <Router>
@@ -59,18 +26,7 @@ function App() {
         <Route path="/location" element={<LocationDecoder />} />
           <Route path="/crime-prediction" element={<Video />} />
         <Route path="/video" element={<VideoCapture />} />
-         <Route path="/wearable" element={<FitbitIntegration />} />
-        
-          <Route
-            path="/blockchain"
-            element={
-              <div className="container">
-                <IncidentForm  />
-                <IncidentViewer />
-              </div>
-            }
-          />
-       
+         <Route path="/wearable" element={<FitbitIntegration />} /> 
       </Routes>
       </div>
     </Router>
